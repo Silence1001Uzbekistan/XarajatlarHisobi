c9643f4c02bb90d49ca269be639342677f1cdf23
Doniyor Artikov <doniyorartikov1999@gmail.com>
2023-02-08 10:18:58 -0800

    11:18

--- app/src/main/java/com/example/xarajatlarhisobi/LoginActivity.kt
+++ app/src/main/java/com/example/xarajatlarhisobi/LoginActivity.kt
@@ -1,19 +1,58 @@
 package com.example.xarajatlarhisobi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.xarajatlarhisobi.Database.AppDatabase
+import com.example.xarajatlarhisobi.Models.Report
+import com.example.xarajatlarhisobi.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+    lateinit var appDatabase: AppDatabase
+
+    lateinit var FullList: ArrayList<Report>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater, null, false)
+        setContentView(binding.root)
 
         supportActionBar!!.hide()
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+
+        appDatabase = AppDatabase.getInstance(this)
+
+        FullList = ArrayList()
+
+        FullList = appDatabase.reportDao().getAllReport() as ArrayList<Report>
+
+
+        binding.btnLogin.setOnClickListener {
+
+
+            for (report in FullList) {
+
+
+                if (report.username == binding.etUsername.text.toString() && report.password == binding.etPassword.text.toString()) {
+
+                    startActivity(Intent(this, HomeActivity::class.java))
+
+                } else {
+                    Toast.makeText(this, "Ma'lumotlarni to'g'ri kiriting", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            }
+
+        }
 
 
     }
